41e269472f1bba75ceb0fef7385f44bda9f2bef4
Charles Giese <charlesg@blc00.bl.pvt>
2020-09-15 15:49:01 +0100

    final

--- turboSETI/BL_event_plot.py
+++ turboSETI/BL_event_plot.py
@@ -1,58 +1,26 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-from turbo_seti import plot_event
-from turbo_seti.find_event.find_event import make_table
 import os
-from turbo_seti.find_event.find_event_pipeline import find_event_pipeline
-import blimpy
-import turbo_seti
-import turbo_seti.find_doppler.seti_event as turbo
-import turbo_seti.find_event as find
+import glob
+from turbo_seti.find_event.plot_event_pipeline import plot_event_pipeline
+
+path='/home/charlesg/turboSETI/trappist1/fil_files/high/'
 
 
-path='/home/charlesg/turboSETI/oumuamua_files/'
-png_path='/home/charlesg/turboSETI/'
+filelist = glob.glob(path+'*.fil')
 
-fil_files=[path+'fil_files'+'OUMUAMUA_0011.gpuspec.0002.fil',
-           path+'fil_files'+'OUMUAMUA_OFF_0012.gpuspec.0002.fil',
-           path+'fil_files'+'OUMUAMUA_0013.gpuspec.0002.fil',
-           path+'fil_files'+'OUMUAMUA_OFF_0014.gpuspec.0002.fil',
-           path+'fil_files'+'OUMUAMUA_0015.gpuspec.0002.fil',
-           path+'fil_files'+'OUMUAMUA_OFF_0016.gpuspec.0002.fil']
+with open('fil_files.lst', 'w') as f:
+    for item in filelist:
+        f.write("%s\n" % item)
 
-dat_files=[path+'OUMUAMUA_0011.gpuspec.0002.dat',
-           path+'OUMUAMUA_OFF_0012.gpuspec.0002.dat',
-           path+'OUMUAMUA_0013.gpuspec.0002.dat',
-           path+'OUMUAMUA_OFF_0014.gpuspec.0002.dat',
-           path+'OUMUAMUA_0015.gpuspec.0002.dat',
-           path+'OUMUAMUA_OFF_0016.gpuspec.0002.dat']
+threshold=3
+snr=10
+target='TRAPPIST1'
 
+csv_path='/home/charlesg/turboSETI/trappist1/'
+csv_string='DIAG_'+target+'_f'+str(threshold)+'_snr'+str(snr)+'.csv'
 
-check_zero_drift = True
-filter_threshold = 1
-SNR_cut = 5
-
-
-event_dataframe = find.find_events(dat_files, 
-                                   SNR_cut=SNR_cut, 
-                                   check_zero_drift=check_zero_drift, 
-                                   filter_threshold=filter_threshold, 
-                                   on_off_first='ON')
-
-event_csv_string='OUMUAMUA_events__f'+str(filter_threshold)+'_SNR'+str(SNR_cut)+'.csv'
-
-print(event_dataframe)
-event_dataframe.to_csv(path+event_csv_string)
-fils_list_string = "fil_files.lst" #just a file containing the .fil names
-turbo_seti.plot_event_pipeline.plot_event_pipeline(event_csv_string, 
-                                                   fil_files,  
-                                                   user_validation=False,
-                                                   offset=0,
-                                                   plot_snr=False)
-
-
-
-
-
-#
+plot_event_pipeline(csv_string,
+                    'fil_files.lst',
+                    user_validation=True)
